95e361557a6c80d4f4dc5fa40b53ae1e8dd90f48
michelaguilera <michelaguilera01@gmail.com>
2022-05-08 13:07:05 +0200

    c

---
+++ TESTING/rich_test.py
@@ -0,0 +1,6 @@
+from rich.console import console
+
+console.print([1, 2, 3])
+console.print("[blue underline]Looks like a link")
+console.print(locals())
+console.print("FOO", style="white on blue")
